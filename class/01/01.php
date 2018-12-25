30b6a82c1e0606c1615c40de9133b635f39f4df1
CongNgoc <ngoccong11a3@gmail.com>
2018-12-25 14:25:32 +0700

    Modify file Database.class.php & 01.php

--- class/01/01.php
+++ class/01/01.php
@@ -2,9 +2,20 @@
 
 	require_once 'Database.class.php';
 
-	$database = new Database();
+	$params = array(
+					'server' 	=> 'localhost',
+					'username'	=> 'root',
+					'password'	=> '',
+					'database'	=> 'db_user',
+					'table'		=> 'group'
+	);
 
+	$database = new Database($params);
 
+	$arrData = array('name'=>'Member 1234', 'status' => 0, 'ordering' => 9);
+
+	$arr_result = $database->insertData($arrData);
+	
 	echo '<pre>';
-	print_r($database);
+	print_r($arr_result);
 	echo '</pre>';
--- class/01/Database.class.php
+++ class/01/Database.class.php
@@ -2,32 +2,86 @@
 
 	class Database
 	{
-		protected $_link;
-		protected $_result;
-		protected $_numRows;
+		protected $_connect;
+		protected $_database;
+		protected $_table;
+		protected $_resultQuery;
 
-		private $_host = "host";
-		private $_username = "root";
-		private $_password = " ";
-		private $_database = "";
-
+		// CONNECT DATABASE
 		// Establish connection to database, when class is instantiated
-		public function __construct()
+		public function __construct($params)
 		{
-			$this->_link = new mysqli($this->host, $this->username, $this->password, $this->database);
+			$_link = new mysqli($params['server'], $params['username'], $params['password'], $params['database']);
 			if(mysqli_connect_errno()) {
 				echo "Connection Failed: " . mysqli_connect_errno();
 				exit();
 			}
+			else
+			{
+				$this->_connect 	= $_link;
+				$this->_database 	= $params['database'];
+				$this->_table 		= $params['table'];
+			}
 		}
 
-		// Sends the query to the connection
-		public function query($sql)
+		//SET CONNECT
+		public function setConnect($connect)
 		{
-			$this->_result = $this->_link->query($sql) or die (mysqli_error($this->_result));
-			return $this->_result;
+			$this->_connect = $connect;
 		}
 
-		// Inserts into databse
+		// SET CONNECT
+		public function setDatabase($database = null)
+		{
+			if($database != null)
+			{
+				$this->_database = $database;
+			}
+			$this->_connect->select_db($this->_database);
+		}
+
+		//SET TABLE
+		public function setTable($table)
+		{
+			$this->_table = $table;
+		}
+
+		//DISCONNECT DATABASE
+		public function __destruct()
+		{
+			$this->_connect->close();
+		}
+
+		//INSERT DATA
+		public function insertData($data)
+		{
+			$newQuery 	= $this->createQueryInsert($data);
+			$query 		= "INSERT INTO `$this->_table`(". $newQuery['cols'] .") VALUES (". $newQuery['vals'].");";
+			$this->query($query);
+		}
 		
+		//CREATE QUERY INSERT
+		public function createQueryInsert($data)
+		{
+			$cols = "";
+			$vals = "";
+			$newQuery = array();
+			if(!empty($data))
+			{
+				foreach ($data as $key => $value) {
+					$cols .= ", `$key`";
+					$vals .= ", '$value'";
+				}
+			}
+			$newQuery['cols'] = substr($cols, 2);
+			$newQuery['vals'] = substr($vals, 2);
+			return $newQuery;
+		}
+
+		//QUERY TO DATABASE
+		public function query($query)
+		{
+			$this->_resultQuery = $this->_connect->query($query);
+			return $this->_resultQuery;
+		}
 	}
